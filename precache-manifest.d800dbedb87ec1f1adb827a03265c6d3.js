e0d7fbe2ab6938ef72e39d57929031ee18dd09e7
Adam <golota.adam@gmail.com>
2026-04-14 02:41:49 +0300

    Updates

---
+++ precache-manifest.d800dbedb87ec1f1adb827a03265c6d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01c34388de81a20ca78a917b4e9e4c3",
+    "url": "/pitch/index.html"
+  },
+  {
+    "revision": "9027cdd5cea53958dd97",
+    "url": "/pitch/static/css/main.866b4e6a.chunk.css"
+  },
+  {
+    "revision": "022149825e11a93f4071",
+    "url": "/pitch/static/js/2.b8693a52.chunk.js"
+  },
+  {
+    "revision": "e939945e62d78c11ba713f1fbef69a8d",
+    "url": "/pitch/static/js/2.b8693a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9027cdd5cea53958dd97",
+    "url": "/pitch/static/js/main.de0e58e8.chunk.js"
+  },
+  {
+    "revision": "d396f0b7cc4a5fea4199",
+    "url": "/pitch/static/js/runtime-main.8ab9dabb.js"
+  },
+  {
+    "revision": "002288e72bb01884587a2f3140727946",
+    "url": "/pitch/static/media/flat.002288e7.svg"
+  },
+  {
+    "revision": "acaa6129ff1059f56c439b59743e05f3",
+    "url": "/pitch/static/media/g.acaa6129.svg"
+  },
+  {
+    "revision": "155dc3c8204711682e57094c5a49cb0c",
+    "url": "/pitch/static/media/note.155dc3c8.svg"
+  },
+  {
+    "revision": "51fa938aabeceb51d69a0d686e8bc29f",
+    "url": "/pitch/static/media/sharp.51fa938a.svg"
+  }
+]);
